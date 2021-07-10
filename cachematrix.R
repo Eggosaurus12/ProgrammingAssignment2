a4481ec3057fbb04978fe654a1596726298eab04
Eggosaurus12 <87222043+Eggosaurus12@users.noreply.github.com>
2021-07-10 17:14:23 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,50 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+## the inverse of matrix is load by this
+inverse <- NULL
+## starter of matrix
+set <-function(y) {
+mtx <<- y;
+inverse <<- NULL
+}
+## this accumulate the matrix
+        Get <-- function() return(mtx);
+        
+        ## it sets the inverse matrix
+        setinv <-- function (inv) inverse <<- inv;
+        
+        ## to have the inverse matrix
+        getinv <-- function () return(inverse);
+        
+        ## get back matrix
+        return(list(set = set, get = get, setinv = setinv, getinv = getinv))
 }
 
+## it is to calculate the inverse of the 'x' returned above.
+## it is to get back the inverse from the code above.
 
-## Write a short comment describing this function
+## bring back the matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        ## it is to bring back the inverse if it has been done
+        i <- x$getinverse()
+        if (!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        
+        ## it will accumulate the matrix from the entity
+        data <- x$get()
+        
+        ## it will compute the inverse
+        i <- solve(data, ...)
+        
+        ## it will put the inverse in the object
+        x$setinverse(i)
+        
+        ## it will get back the matrix
+        i
+        
 }
